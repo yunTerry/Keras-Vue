8eb19646de865dc8e91621f8135e8a729e5cd94e
Terry <2219569347@qq.com>
2018-08-01 17:51:48 +0800

    pick photo

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,127 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="main">
+    <div id="left">
+      <div id="pick">
+          <a href="javascript:;" class="pickfile">选取图片
+              <input type="file" accept="image/*" @change="preview($event)">
+          </a>
+      </div>
+      <div id="result">
+           <ve-bar :data="chartData" :settings="chartSettings"></ve-bar>
+      </div>
+    </div>
+    <div id="right" class="imgBox">
+      <div class="inbox">
+          <img :src="imgurl">
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  data() {
+    this.chartSettings = {
+      labelMap: {
+        label: "标签",
+        probability: "概率"
+      },
+      dataOrder: {
+        label: "probability",
+        order: "desc"
+      }
+    };
+    return {
+      imgurl: "",
+      chartData: {
+        columns: ["label", "probability"],
+        rows: []
+      }
+    };
+  },
+  methods: {
+    preview(event) {
+      let _this = this;
+      var image = event.target.files[0];
+      if (!event || !window.FileReader) return;
+      let reader = new FileReader();
+      reader.readAsDataURL(image);
+      reader.onloadend = function() {
+        _this.imgurl = this.result;
+      };
+      let fdata = new FormData();
+      fdata.append("image", image);
+      this.$http
+        .post("/predict", fdata, {
+          headers: { "Content-Type": "multipart/form-data" }
+        })
+        .then(function(res) {
+          if (res.data.success) {
+            _this.chartData.rows = res.data.predictions;
+          }
+        });
+    }
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+#main {
+  margin: 1rem;
+}
+.imgBox {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.inbox {
+  margin: 3rem;
+}
+.inbox img {
+  max-width: 100%;
+  max-height: 100%;
+}
+#left {
+  width: 50%;
+  height: 600px;
+  float: left;
+}
+#pick {
+  height: 30%;
+}
+#result {
+  height: 70%;
+}
+#right {
+  width: 50%;
+  height: 600px;
+  float: right;
+}
+.pickfile {
+  margin-left: 30%;
+  margin-top: 10%;
+  position: relative;
+  display: inline-block;
+  background: #d0eeff;
+  border: 1px solid #99d3f5;
+  border-radius: 5px;
+  padding: 8px 18px;
+  overflow: hidden;
+  color: #1024be;
+  text-decoration: none;
+  text-indent: 0;
+  font: 1.2rem sans-serif;
+}
+.pickfile input {
+  position: absolute;
+  font-size: 100px;
+  right: 0;
+  top: 0;
+  opacity: 0;
+}
+.pickfile:hover {
+  background: #aadffd;
+  border-color: #78c3f3;
+  color: #004974;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axios from 'axios'
+import VCharts from 'v-charts'
+
+Vue.use(VCharts)
+
+axios.defaults.baseURL = 'http://localhost:5000/'
+axios.defaults.headers.post['Content-Type'] = 'multipart/json;charset=UTF-8'
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App)
 }).$mount('#app')
+
