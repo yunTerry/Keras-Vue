8187d3810c3b75b87f5944dd1535cccdbe887856
Terry <2219569347@qq.com>
2018-08-11 17:49:42 +0800

    选图片子组件

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,10 @@
 <template>
     <div>
         <div class="left">
-            <div class="pick">
-                <a href="javascript:" class="pickfile">选取图片
-                    <input type="file" accept="image/*" @change="preview($event)">
-                </a>
-            </div>
-            <div class="result">
-                <ve-bar v-if="ifshow" :data="chartData" :settings="chartSettings"></ve-bar>
-            </div>
+            <pick @getimage="useimage"></pick>
+            <ve-bar class="result" v-if="ifshow"
+                    :data="chartData" :settings="chartSettings">
+            </ve-bar>
         </div>
         <div class="right">
             <div class="imgbox">
@@ -19,7 +15,13 @@
 </template>
 
 <script>
+
+    import pickPhoto from "./components/pickPhoto";
+
     export default {
+        components: {
+            pick: pickPhoto
+        },
         data() {
             this.chartSettings = {
                 labelMap: {
@@ -41,8 +43,7 @@
             };
         },
         methods: {
-            preview(event) {
-                let image = event.target.files[0];
+            useimage(image) {
                 this.imgurl = window.URL.createObjectURL(image);
                 this.postimg(image);
             },
@@ -72,42 +73,8 @@
         float: left;
     }
 
-    .pick {
-        height: 30%;
-    }
-
     .result {
         height: 70%;
-    }
-
-    .pickfile {
-        margin-left: 20%;
-        margin-top: 10%;
-        position: relative;
-        display: inline-block;
-        background: #d0eeff;
-        border: 1px solid #99d3f5;
-        border-radius: 5px;
-        padding: 8px 18px;
-        overflow: hidden;
-        color: #1024be;
-        text-decoration: none;
-        text-indent: 0;
-        font: 1.2rem sans-serif;
-    }
-
-    .pickfile input {
-        position: absolute;
-        font-size: 100px;
-        right: 0;
-        top: 0;
-        opacity: 0;
-    }
-
-    .pickfile:hover {
-        background: #aadffd;
-        border-color: #78c3f3;
-        color: #004974;
     }
 
     .right {
---
+++ src/components/pickPhoto.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="pick">
+        <a href="javascript:" class="pickfile">选取图片
+            <input type="file" accept="image/*" @change="preview($event)">
+        </a>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "pickPhoto",
+        methods: {
+            preview(event) {
+                let image = event.target.files[0];
+                this.$emit("getimage", image)
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .pick {
+        height: 30%;
+    }
+
+    .pickfile {
+        margin-left: 20%;
+        margin-top: 10%;
+        position: relative;
+        display: inline-block;
+        background: #d0eeff;
+        border: 1px solid #99d3f5;
+        border-radius: 5px;
+        padding: 8px 18px;
+        overflow: hidden;
+        color: #1024be;
+        text-decoration: none;
+        text-indent: 0;
+        font: 1.2rem sans-serif;
+    }
+
+    .pickfile input {
+        position: absolute;
+        font-size: 100px;
+        right: 0;
+        top: 0;
+        opacity: 0;
+    }
+
+    .pickfile:hover {
+        background: #aadffd;
+        border-color: #78c3f3;
+        color: #004974;
+    }
+</style>
